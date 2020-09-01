f285f143f63906db8a516f39d3f9579a34409029
Brian Goldman <leonel89011@gmail.com>
2020-08-31 21:28:23 -0300

    Spring Initializr

---
+++ backend-desapp-api/src/test/java/ar/edu/unq/desapp/grupon/backenddesappapi/BackendDesappApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.desapp.grupon.backenddesappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDesappApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
