0e368c92b393f747c454e864347541e4b4dca5ba
Brian Goldman <leonel89011@gmail.com>
2020-09-06 22:00:58 -0300

    Changed oraclejdk for openjdk in .travis.yml

--- backend-desapp-api/src/main/java/ar/edu/unq/desapp/grupoN022020/backenddesappapi/BackendDesappApiApplication.java
+++ backend-desapp-api/src/main/java/ar/edu/unq/desapp/grupoN022020/backenddesappapi/BackendDesappApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendDesappApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendDesappApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendDesappApiApplication.class, args);
+    }
 
 }
