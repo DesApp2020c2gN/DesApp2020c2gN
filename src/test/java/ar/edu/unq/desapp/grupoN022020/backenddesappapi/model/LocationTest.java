278e9bd04c933231fd10a054c6fb05a93e4407f7
Brian Goldman <leonel89011@gmail.com>
2020-09-07 19:16:01 -0300

    Added LocationBuilder

--- src/test/java/ar/edu/unq/desapp/grupoN022020/backenddesappapi/model/LocationTest.java
+++ src/test/java/ar/edu/unq/desapp/grupoN022020/backenddesappapi/model/LocationTest.java
@@ -2,22 +2,40 @@ package ar.edu.unq.desapp.grupoN022020.backenddesappapi.model;
 
 import org.junit.jupiter.api.Test;
 
+import static ar.edu.unq.desapp.grupoN022020.backenddesappapi.model.LocationBuilder.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LocationTest {
 
     @Test
-    public void test_Constructor(){
-        String name = "name";
-        String province = "province";
-        int population = 1000;
-        String state = "state";
-
-        Location location = new Location(name, province, population, state);
+    public void test_LocationName(){
+        String name = "Piedras";
+        Location location = aLocation().withName(name).build();
 
         assertEquals(location.getName(), name);
+    }
+
+    @Test
+    public void test_LocationProvince(){
+        String province = "Catamarca";
+        Location location = aLocation().withProvince(province).build();
+
         assertEquals(location.getProvince(), province);
+    }
+
+    @Test
+    public void test_LocationPopulation(){
+        int population = 1250;
+        Location location = aLocation().withPopulation(population).build();
+
         assertEquals(location.getPopulation(), population);
+    }
+
+    @Test
+    public void test_LocationState(){
+        String state = "Conectado";
+        Location location = aLocation().withState(state).build();
+
         assertEquals(location.getState(), state);
     }
 }
