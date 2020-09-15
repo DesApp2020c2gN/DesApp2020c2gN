3b8d29e454609bb72dc8d428066f133e944bda59
Brian Goldman <leonel89011@gmail.com>
2020-09-14 23:19:01 -0300

    Code quality on imports on UserTest

--- src/test/java/ar/edu/unq/desapp/grupon022020/backenddesappapi/model/UserTest.java
+++ src/test/java/ar/edu/unq/desapp/grupon022020/backenddesappapi/model/UserTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
