99070588cbc4e3a8eeea10649ea26a7adb09dce7
Brian Goldman <leonel89011@gmail.com>
2020-10-17 19:30:46 -0300

    Moved method loginUser to UserRepository

--- src/main/java/ar/edu/unq/desapp/grupon022020/backenddesappapi/persistence/UserRepository.java
+++ src/main/java/ar/edu/unq/desapp/grupon022020/backenddesappapi/persistence/UserRepository.java
@@ -2,6 +2,7 @@ package ar.edu.unq.desapp.grupon022020.backenddesappapi.persistence;
 
 import ar.edu.unq.desapp.grupon022020.backenddesappapi.model.DonorUser;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,12 +11,15 @@ import java.util.Optional;
 
 @Configuration
 @Repository
-public interface UserRepository extends CrudRepository<DonorUser, String>, CustomizedUserRepository {
+public interface UserRepository extends CrudRepository<DonorUser, String> {
 
     boolean existsById(String name);
 
     Optional<DonorUser> findById(String id);
 
     List<DonorUser> findAll();
+
+    @Query("SELECT u FROM DonorUser u WHERE u.nickname=?1 and u.password=?2")
+    DonorUser loginUser(String nickname, String password);
 
 }
